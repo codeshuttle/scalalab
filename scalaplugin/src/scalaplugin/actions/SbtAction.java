3beed718d8a3ebc70cd42ddac20265dadc3de28a
codeshuttle <parthipan.p@imaginea.com>
2013-12-04 15:24:13 +0530

    Added sbt intrepreter

--- scalaplugin/src/scalaplugin/actions/SbtAction.java
+++ scalaplugin/src/scalaplugin/actions/SbtAction.java
@@ -14,6 +14,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Stack;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -38,6 +39,10 @@ import scalaplugin.Console;
  */
 public class SbtAction implements IWorkbenchWindowActionDelegate {
 
+	// private static final File errorFile = new
+	// File("D:\\ws\\tmp\\log_error.txt");
+	// private static final File messageFile = new
+	// File("D:\\ws\\tmp\\log_info.txt");
 	private IWorkbenchWindow activeWindow = null;
 	private Shell shell = null;
 	private Process sbtProcess = null;
@@ -46,30 +51,33 @@ public class SbtAction implements IWorkbenchWindowActionDelegate {
 	private InputStream inputStream = null;
 	private OutputStream outputStream = null;
 	private ScheduledFuture<?> scheduleAtFixedRate = null;
-	
+	private final ExecutorService service = Executors.newSingleThreadExecutor();
+	private Text error = null;
+	private Text message = null;
 	private Collection<Character> charEntered = new ArrayList<>();
 	private Stack<String> commands = new Stack<>();
-	
+
 	private final KeyListener listener = new KeyListener() {
-		
+
 		@Override
 		public void keyReleased(KeyEvent e) {
-			if(e.keyCode==13){
-				Character[] arr = (Character[])charEntered.toArray();
+			if (e.keyCode == 13) {
+				Character[] arr = (Character[]) charEntered.toArray();
 				String command = new String(convert(arr));
 				commands.push(command);
 				charEntered.clear();
 				runCommand();
-			}else{
+			} else {
 				charEntered.add(e.character);
 			}
 		}
-		
+
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+
 		}
 	};
+
 	/**
 	 * 
 	 */
@@ -77,59 +85,111 @@ public class SbtAction implements IWorkbenchWindowActionDelegate {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void run(IAction action) {
-		try {
-			if (activeWindow == null) {
-				sbtProcess = Runtime.getRuntime().exec("sbt");
+	private final Runnable initializer = new Runnable() {
+
+		@Override
+		public void run() {
+			try {
+				// refresh(errorFile,messageFile);
+
+				ProcessBuilder pb = new ProcessBuilder("sbt");
+
+				// pb.redirectErrorStream(true);
+				// pb.redirectError(Redirect.appendTo(errorFile));
+				// pb.redirectOutput(Redirect.appendTo(messageFile));
+				//
+				//
+				// WatchService watcher =
+				// FileSystems.getDefault().newWatchService();
+				// Path errorpath =
+				// FileSystems.getDefault().getPath(errorFile.getParent(),
+				// errorFile.getName());
+				// Path messagepath =
+				// FileSystems.getDefault().getPath(messageFile.getParent(),
+				// messageFile.getName());
+				//
+				// WatchKey errorWatchKey = errorpath.register(watcher,
+				// StandardWatchEventKinds.ENTRY_MODIFY);
+				// WatchKey messageWatchKey = messagepath.register(watcher,
+				// StandardWatchEventKinds.ENTRY_MODIFY);
+
+				sbtProcess = pb.start();
+
 				errorStream = sbtProcess.getErrorStream();
 				inputStream = sbtProcess.getInputStream();
 				outputStream = sbtProcess.getOutputStream();
+
 				thread = Executors.newScheduledThreadPool(1);
-				scheduleAtFixedRate = thread.scheduleAtFixedRate(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							Text error = new Text(shell, SWT.ERROR);
-							error.setText(fromStream(errorStream));
-							error.pack();
-							Text message = new Text(shell, SWT.ALL);
-							message.setText(fromStream(inputStream));
-							message.pack();
-						} catch (IOException e) {
-							handleException(e);
-						}
-					}
-				}, 5, 5, TimeUnit.SECONDS);
-				
+				error = new Text(shell, SWT.READ_ONLY | SWT.MULTI | SWT.ERROR);
+				message = new Text(shell, SWT.READ_ONLY | SWT.MULTI | SWT.None);
+				scheduleAtFixedRate = thread.scheduleAtFixedRate(
+						new Runnable() {
+							@Override
+							public void run() {
+								try {
+									error.append(fromStream(errorStream));
+									message.append(fromStream(inputStream));
+								} catch (IOException e) {
+									handleException(e);
+								}
+							}
+						}, 5, 5, TimeUnit.SECONDS);
+
 				shell.addKeyListener(listener);
-			} else {
-				shell.setActive();
+			} catch (IOException e) {
+				handleException(e);
+				sbtProcess = null;
 			}
-		} catch (IOException e) {
-			handleException(e);
-			sbtProcess = null;
+
+		}
+	};
+
+	// private void refresh(File... arr){
+	// for(File f:arr){
+	// if(f.exists()){
+	// f.delete();
+	// try {
+	// f.createNewFile();
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// }
+	// }
+	//
+	// private volatile int errorSize = 0;
+	// private volatile int messageSize = 0;
+
+	@Override
+	public void run(IAction action) {
+		if (thread == null) {
+//			shell.open();
+			service.execute(initializer);
+			message.append("sbt started!");
+			// message.pack();
+		} else {
+			shell.setActive();
 		}
 	}
 
 	private void runCommand() {
 		try {
-			String command = commands.peek()+"\n";
+			String command = commands.peek() + "\n";
 			outputStream.write(command.getBytes(Charset.defaultCharset()));
-			Console.log("Executed command '"+command+"'");
+			Console.log("Executed command '" + command + "'");
 		} catch (IOException e) {
 			handleException(e);
 		}
 	}
 
-	private char[] convert(Character[] arr){
+	private char[] convert(Character[] arr) {
 		char[] carr = new char[arr.length];
-		for(int i=0;i<arr.length;i++){
+		for (int i = 0; i < arr.length; i++) {
 			carr[i] = arr[i];
 		}
 		return carr;
 	}
-	
+
 	private void handleException(IOException e) {
 		StringWriter sw = new StringWriter();
 		e.printStackTrace(new PrintWriter(sw));
@@ -139,18 +199,17 @@ public class SbtAction implements IWorkbenchWindowActionDelegate {
 		exceptionTrace.pack();
 		MessageDialog.openInformation(shell, "Scala Sbt", string);
 	}
-	
-	String fromStream(InputStream in) throws IOException
-	{
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-	    StringBuilder out = new StringBuilder();
-	    String line;
-	    while ((line = reader.readLine()) != null) {
-	        out.append(line);
-	    }
-	    return out.toString();
+
+	String fromStream(InputStream in) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		StringBuilder out = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			out.append(line);
+		}
+		return out.toString();
 	}
-	
+
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
 		// TODO Auto-generated method stub
@@ -159,13 +218,16 @@ public class SbtAction implements IWorkbenchWindowActionDelegate {
 
 	@Override
 	public void dispose() {
-		if(thread!=null){
+		if (thread != null) {
 			shell.removeKeyListener(listener);
 			scheduleAtFixedRate.cancel(true);
 			thread.shutdownNow();
 			errorStream = null;
 			inputStream = null;
 			outputStream = null;
+			error = null;
+			message = null;
+			thread = null;
 		}
 	}
 
